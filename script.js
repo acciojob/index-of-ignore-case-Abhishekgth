2354649a8496d197b4f1f1754c4f45151973db7d
Abhishekgth <128219073+Abhishekgth@users.noreply.github.com>
2024-01-19 17:29:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	 const lowerStr = s1.toLowerCase();
+     const lowerSubStr = s2.toLowerCase();
+
+	 const index = lowerStr.indexOf(lowerSubStr);
+
+	 if (index === -1) {
+    return -1;
+  }
+
+	 return index;
 }
 
 // Please do not change the code below
